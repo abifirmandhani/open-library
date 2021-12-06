8e413fb5a3ebf650e66071c00d342cc925bb30b0
Abi firmandhani <82786813+AbiFirmandhani-Grune@users.noreply.github.com>
2021-12-06 15:23:34 +0800

    first web server

--- main.go
+++ main.go
@@ -1,11 +1,33 @@
 package main
 
 import (
-	"first-api/library"
+	"fmt"
 	"net/http"
+	"html/template"
 )
 
+func index(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintln(w, "apa kabar!")
+}
+
 func main()  {
-	fmt.Println("Name", library.Student.Name)
-	fmt.Println("Grade", library.Student.Grade)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+		var data = map[string]string{
+			"Name": "Abi F",
+			"Message": "thank you",
+		}
+
+		var t, err = template.ParseFiles("template.html")
+		if err != nil{
+			fmt.Println(err.Error())
+			return
+		}
+
+		t.Execute(w, data)
+	})
+
+	fmt.Println("starting listening ")
+	http.ListenAndServe(":8000", nil)
+
 }
